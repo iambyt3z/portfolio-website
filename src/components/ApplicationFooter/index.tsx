71f8a844d8deb751e6903ea6683fb67f771a07ad
Siddhant Thakare <siddhantthakare7@gmail.com>
2023-12-31 16:24:08 -0800

    Created Footer component

---
+++ src/components/ApplicationFooter/index.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import { DARK_COLOR } from "../../constant";
+
+const ApplicationFooter = () => {
+    return (
+        <Box
+            height="200px"
+            width="100%"
+            sx={{ "background": DARK_COLOR }}
+        >
+        
+        </Box>
+    );
+};
+
+export default ApplicationFooter;
